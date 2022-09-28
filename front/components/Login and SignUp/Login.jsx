dbc0b82b46032df24a98d949f8460491f6766c9b
icarodr <it46009@gmail.com>
2022-09-28 16:58:22 -0300

    add new login page and other styles from pages

--- front/components/Login and SignUp/Login.jsx
+++ front/components/Login and SignUp/Login.jsx
@@ -1,11 +1,20 @@
 const Login = () => {
     return(
-        <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-cover custom-img">
-          <div className="top-0 left-0 right-0 bottom-0 z-[2] custom-img z-2"/>
-          <div className="absolute p-5 z-[2]  mt-[-10rem]">
-          </div>
+      <>
+      <div className="bg-none flex flex-col rounded-sm">
+        <h1 className="text-white">Login</h1>
+        <input type="text" className="mb-10 bg-transparent border rounded-sm" />
+        <h1 className="text-white">Senha</h1>
+        <input type="password" className="mb-10 bg-transparent border rounded-sm" />
+
+        <div className="text-white">
+        <button className="px-8 py-2 border hover:bg-gray-600/30 mr-6">Login</button>
+        <button className="px-8 py-2 border hover:bg-gray-600/30">SignUp</button>
       </div>
-    )
+
+      </div>
+      </>
+  )
 }
 
 export default Login;
--- front/pages/login.js
+++ front/pages/login.js
@@ -1,10 +1,12 @@
 import Link from "next/link"
+import Login from "../components/Login and SignUp/Login"
 
-export default function Login() {
+export default function Login_Page() {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-cover">
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-cover custom-img2">
           <div className="top-0 left-0 right-0 bottom-0 z-[2] custom-img z-2"/>
           <div className="absolute p-5 z-[2]  mt-[-10rem]">
+            <Login/>
           </div>
       </div>
   )
