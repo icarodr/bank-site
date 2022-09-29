c11fad13851a6d5cc3f16224c9bf0e9e812a820e
icarodr <it46009@gmail.com>
2022-09-28 23:34:08 -0300

    add new login and signup pages

--- front/components/Login and SignUp/Login.jsx
+++ front/components/Login and SignUp/Login.jsx
@@ -1,19 +1,16 @@
 const Login = () => {
     return(
-      <>
-      <div className="bg-none flex flex-col rounded-sm">
-        <h1 className="text-white">Login</h1>
-        <input type="text" className="mb-10 bg-transparent border rounded-sm" />
-        <h1 className="text-white">Senha</h1>
-        <input type="password" className="mb-10 bg-transparent border rounded-sm" />
-
-        <div className="text-white">
-        <button className="px-8 py-2 border hover:bg-gray-600/30 mr-6">Login</button>
-        <button className="px-8 py-2 border hover:bg-gray-600/30">SignUp</button>
-      </div>
-
-      </div>
-      </>
+    <div className="container h-96 w-96 bg-white bg-opacity-10 relative z-2 rounded-2xl border border-r-0 border-b-0 border-gray-200/25 drop-shadow-2xl backdrop-filter backdrop-blur-sm">
+      <form className="h-full flex flex-col justify-evenly items-center">
+        <div className="text-white text-2xl tracking-wider">Login</div>
+        <input type="username" placeholder="Login" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide"/>
+        <input type="password" placeholder="Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide"/>
+        <div className="">
+          <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border mr-6">Login</button>
+          <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border">SignUp</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
---
+++ front/pages/signup.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import Register from "../components/Login and SignUp/SignUp"
+
+export default function SignUp_Page() {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-cover custom-img2">
+          <div className="top-0 left-0 right-0 bottom-0 z-[2] custom-img z-2"/>
+          <div className="absolute p-5 z-[2]  mt-[-10rem]">
+            <Register/>
+          </div>
+      </div>
+  )
+}
