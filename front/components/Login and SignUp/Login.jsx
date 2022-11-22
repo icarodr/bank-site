52fed895cd1d12f8f745b2cb03fa9a797b55c13f
icarodr <it46009@gmail.com>
2022-11-22 16:59:28 -0300

    correct signup page

--- front/components/Login and SignUp/Login.jsx
+++ front/components/Login and SignUp/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
         <div className="text-white text-2xl tracking-wider">Login</div>
         <input type="username" placeholder="Login" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide"/>
         <input type="password" placeholder="Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide"/>
-        <div className="">
+        <div>
           <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border mr-6">Login</button>
           <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border">SignUp</button>
         </div>
--- front/components/Login and SignUp/SignUp.jsx
+++ front/components/Login and SignUp/SignUp.jsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 const Register = () => {
  return(
-    <div className="container h-96 w-96 bg-white bg-opacity-10 relative z-2 rounded-2xl border border-r-0 border-b-0 border-gray-200/25 drop-shadow-2xl backdrop-filter backdrop-blur-sm">
-    <form className="h-full flex flex-col justify-evenly items-center">
-        <div className="text-white text-2xl tracking-wider">Register</div>
-            <input type="username" placeholder="Login" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide "/>
-            <input type="email" placeholder="E-mail" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide"/>
-            <input type="username" placeholder="Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide "/>
-            <input type="email" placeholder="Repita a Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide"/>
-        <div className="">
-            <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border mr-6">Login</button>
-            <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border">SignUp</button>
+    <div className="bg-white bg-opacity-10 rounded-2xl border border-r-0 border-b-0 border-gray-200/25 drop-shadow-2xl backdrop-filter backdrop-blur-sm">
+      <form className="m-16 flex flex-col items-center">
+        <div className="text-white text-2xl tracking-wider mb-8">Register</div>
+        <input type="username" placeholder="Login" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide mb-10"/>
+        <input type="password" placeholder="Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide mb-10"/>
+        <input type="password" placeholder="Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide mb-10"/>
+        <input type="password" placeholder="Senha" className="w-64 text-white bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 tracking-wide mb-16"/>
+        <div className="mb-8">
+          <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border mr-6">Login</button>
+          <button className="text-white cursor-pointer px-8 py-2 bg-transparent hover:bg-white/20 border">SignUp</button>
         </div>
-    </form>
+      </form>
     </div>
   )
 }
