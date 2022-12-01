db985e729d02cb974a34fa3b41d62fc3ff4dc015
icarodr <it46009@gmail.com>
2022-12-01 11:09:38 -0300

    english content

--- front/components/Center/Center.jsx
+++ front/components/Center/Center.jsx
@@ -7,7 +7,7 @@ const Center = ({titulo, mensagem}) => {
         <div className="absolute p-5 text-white z-[2]  mt-[-10rem]">
             <h2 className="text-5xl">{titulo}</h2>
             <p className="py-5 text-xl">{mensagem}</p>
-            <button className="px-8 py-2 border hover:bg-gray-600/30">Saiba Mais</button>
+            <button className="px-8 py-2 border hover:bg-gray-600/30">View more</button>
         </div>
     </div>
     );
